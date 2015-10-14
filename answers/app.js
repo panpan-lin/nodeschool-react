024274b8f3cc60e6464afd46bd26b3301db3c427
Panpan Lin <panpanli@ie.ibm.com>
2015-10-14 12:00:43 +0100

    remove deprecated API and use react-dom instead

--- answers/app.js
+++ answers/app.js
@@ -1,10 +1,11 @@
 /* This code is for using React at front-end */
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 // passing TodoBox from index.jsx
 var TodoBox = require('./views/index.jsx');
 
 // passing data from server that are passed in the id of initial-data to the element that has the id of app
 var data = JSON.parse(document.getElementById('initial-data').getAttribute('data-json'));
-React.render(<TodoBox data={data} />, document.getElementById("app"));
+ReactDOM.render(<TodoBox data={data} />, document.getElementById("app"));
